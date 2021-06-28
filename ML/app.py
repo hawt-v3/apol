347754a20a1f1634ed1318b40a5990d2af3d4cac
Keval Botadra <keval.botadra@gmail.com>
2021-06-28 11:55:12 -0400

    needed to update the ml file to have the recent code as of 3:00 yesterday

--- ML/app.py
+++ ML/app.py
@@ -1,15 +1,10 @@
 from flask import Flask
 import flask
-from gpt3 import classifyArticle
-
+import gpt3
 app = Flask(__name__)
 
 @app.route("/classify", methods=["POST"])
 def classify():
     content = flask.request.json
-    alignment = classifyArticle(content["content"])
-    return {"Alignment": alignment}
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    alignment = gpt3.classifyArticle(content["content"])
+    return {"alignment": alignment}
